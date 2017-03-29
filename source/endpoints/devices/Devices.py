9439bfc15f6fdc7e71c9e149b3ea92f5281a7048
Suzanne Maquelin <slmaquelin@gmail.com>
2017-03-29 18:11:08 +0200

    post device raises exception in PluginManager

--- source/endpoints/devices/Devices.py
+++ source/endpoints/devices/Devices.py
@@ -2,7 +2,14 @@ from flask_restplus import Resource
 
 from endpoints.devices import device_database, namespace
 from endpoints.devices.Device import device
+from endpoints.util.ToString import ToString
+from endpoints.devices import plugin_manager
 
+format_post_device = namespace.model('Device', {
+    'organization': ToString(readOnly=True, required=True, discription='The organization')
+    ,'plugin_name': ToString(readOnly=True, required=True, description='The name of the plugin')
+    , 'required_info': ToString(attribgute='requiredInfo', required=True, discription='The info needed to operate')
+})
 
 @namespace.route('/')
 class Devices(Resource):
@@ -13,4 +20,13 @@ class Devices(Resource):
         '''List all devices'''
         return device_database.getDevices()
 
-
+    @namespace.doc('post_device')
+    @namespace.expect(format_post_device)
+    @namespace.response(201, 'new device')
+    def post(self):
+        ''' Post a new device '''
+        organization = namespace.apis[0].payload["organization"]
+        plugin_name = namespace.apis[0].payload["plugin_name"]
+        required_info = namespace.apis[0].payload["required_info"]
+        class_plugin = plugin_manager.get_implementation_of(organization, plugin_name, required_info)
+        device_database.addDevice(class_plugin)
--- source/endpoints/devices/__init__.py
+++ source/endpoints/devices/__init__.py
@@ -1,6 +1,9 @@
 from flask_restplus import Namespace
 
 from model.Database import Database
+from model.PluginManager import PluginManager
+
+plugin_manager = PluginManager()
 
 device_database = Database()
 
