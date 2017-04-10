0dff8056cf0933c33dc97148f05b5ffa7f97f836
LarsHoldijk <larsholdijk@gmail.com>
2017-04-10 10:45:47 +0200

    The activator id was not properly set andcolor was names brightness

--- source/plugins/setup/philips/PhilipsHueSetup.py
+++ source/plugins/setup/philips/PhilipsHueSetup.py
@@ -20,12 +20,13 @@ class PhilipsHueSetup(Device):
         return "Setup"
 
     def setup(self):
+
         if self.required_info["user"] == "unknown":
-            #data = '{"devicetype":"hue#whiteLight"}'
-            #response = requests.post(self._baseUrl, data)
-            #message = json.loads(response.content)[0]
-            #succes = message['success']
-            self.required_info["user"] = 'CGxOchdTOaFQx-tUS8q0Orqfr4hYifYXQaRRJwR2'#succes['username']
+            data = '{"devicetype":"hue#PhilipsSetup"}'
+            response = requests.post("http://" + self.required_info["ip"] + "/api", data)
+            message = json.loads(response.content)[0]
+            succes = message['success']
+            self.required_info["user"] = succes['username']
             print(self.id)
             self.required_info["id"] = self.id
 
@@ -35,7 +36,7 @@ class PhilipsHueSetup(Device):
 
     @classmethod
     def _get_extra_required_info(cls) -> dict:
-        return {"ip": "192.168.2.19", "user": "unknown"}
+        return {"ip": "192.168.178.32", "user": "unknown"}
 
     @classmethod
     def _get_plugin_name(cls):
