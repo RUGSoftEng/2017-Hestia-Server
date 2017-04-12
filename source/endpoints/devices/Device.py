ff2f1078768b1f37b7ba84fd47209352471a3662
Suzanne Maquelin <slmaquelin@gmail.com>
2017-04-12 12:45:31 +0200

    changes in documentation and UI

--- source/endpoints/devices/Device.py
+++ source/endpoints/devices/Device.py
@@ -6,7 +6,8 @@ from endpoints.devices.Activator import activator
 from endpoints.devices.Devices import device_database
 
 device = namespace.model('Device', {
-    'deviceId': fields.Integer(attribute="id", readOnly=True, required=True, description='The unique identifier of the device')
+    'deviceId': fields.Integer(attribute='id', readOnly=True, required=True,
+                               description='The unique identifier of the device')
     , 'name': fields.String(required=True, description='The name of the device')
     , 'type': fields.String(attribute='plugin_type',required=True)
     , 'required_info': fields.String(required=True, description='Required info of device')
@@ -18,17 +19,17 @@ device = namespace.model('Device', {
 @namespace.response(404, 'Device not found')
 @namespace.param('device_id', 'The device ID')
 class Device(Resource):
-    """ Show a single device, update and delete a device"""
+    """ Show a single device and delete a device """
 
     @namespace.doc('get_device')
     @namespace.marshal_with(device)
     def get(self, device_id):
-        """ Fetch a given device"""
+        """ Fetch a device given its identifier """
         return device_database.get_device(device_id)
 
     @namespace.doc('delete_device')
     @namespace.response(204, 'Device deleted')
     def delete(self, device_id):
-        """ Delete a device given its identifier"""
+        """ Delete a device given its identifier """
         device_database.delete_device(device_id)
         return '', 204
