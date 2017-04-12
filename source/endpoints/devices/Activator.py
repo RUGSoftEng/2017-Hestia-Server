eabc88bd5d38724d71d26bca4067ca09635c5361
Suzanne Maquelin <slmaquelin@gmail.com>
2017-04-12 17:31:54 +0200

    change of quotation

--- source/endpoints/devices/Activator.py
+++ source/endpoints/devices/Activator.py
@@ -3,41 +3,41 @@ from flask_restplus import fields
 
 from endpoints.devices import namespace, device_database
 
-activator = namespace.model('Activator', {
-    'activatorId': fields.Integer(attribute='id', readOnly=True, required=True,
-                                  description='The unique identifier of the action')
-    , 'name': fields.String(readOnly=True, required=True, description='The name of the action')
-    , 'type': fields.String(attribute='type', readOnly=True, required=True,
-                            description='The type of interaction you can have')
-    , 'state': fields.String(attribute='state', required=True, description='The state of an action')
+activator = namespace.model("Activator", {
+    "activatorId": fields.Integer(attribute="id", readOnly=True, required=True,
+                                  description="The unique identifier of the action")
+    , "name": fields.String(readOnly=True, required=True, description="The name of the action")
+    , "type": fields.String(attribute="type", readOnly=True, required=True,
+                            description="The type of interaction you can have")
+    , "state": fields.String(attribute="state", required=True, description="The state of an action")
 })
-state = namespace.model('State', {
-    'state': fields.String(required=True, description='new state of activator')
+state = namespace.model("State", {
+    "state": fields.String(required=True, description="new state of activator")
 })
 
 
-@namespace.route('/<int:device_id>/activators/<int:activator_id>')
-@namespace.response(404, 'Device or action not found')
-@namespace.param('device_id', 'The device ID')
-@namespace.param('activator_id', 'The action ID')
+@namespace.route("/<int:device_id>/activators/<int:activator_id>")
+@namespace.response(404, "Device or action not found")
+@namespace.param("device_id", "The device ID")
+@namespace.param("activator_id", "The action ID")
 class Activator(Resource):
     """ Show a single action and performs a single action """
 
-    @namespace.doc('get_activator')
+    @namespace.doc("get_activator")
     @namespace.marshal_with(activator)
     def get(self, device_id, activator_id):
         """ Fetch a given action of a device """
         device = device_database.get_device(device_id)
         return device.get_activator(activator_id)
 
-    @namespace.doc('post_activator')
+    @namespace.doc("post_activator")
     @namespace.expect(state)
-    @namespace.response(201, 'state updated')
+    @namespace.response(201, "state updated")
     def post(self, device_id, activator_id):
         """ Post a given action of a device """
         device = device_database.get_device(device_id)
         action = device.get_activator(activator_id)
-        value = namespace.apis[0].payload['state']
+        value = namespace.apis[0].payload["state"]
         action.set_state_with_string(value)
         action.perform(device.required_info)
-        return 'state updated', 201
+        return "state updated", 201
--- source/endpoints/devices/Devices.py
+++ source/endpoints/devices/Devices.py
@@ -5,32 +5,32 @@ from endpoints.devices import device_database, namespace
 from endpoints.devices.Device import device
 from endpoints.devices import plugin_manager
 
-format_post_device = namespace.model('new_device', {
-    'required_info': fields.String(attribute='required_info', required=True,
-                                   description='The organization and plugin_name of device and additional info')
+format_post_device = namespace.model("new_device", {
+    "required_info": fields.String(attribute="required_info", required=True,
+                                   description="The organization and plugin_name of device and additional info")
 })
 
 
-@namespace.route('/')
+@namespace.route("/")
 class Devices(Resource):
     """ Shows a list of all devices and adds a new device with POST """
 
-    @namespace.doc('list_devices')
+    @namespace.doc("list_devices")
     @namespace.marshal_list_with(device)
     def get(self):
         """ List all devices """
         return device_database.get_devices()
 
-    @namespace.doc('post_device')
+    @namespace.doc("post_device")
     @namespace.expect(format_post_device)
-    @namespace.response(201, 'new device')
+    @namespace.response(201, "new device")
     def post(self):
         """ Post a new device """
-        required_info = namespace.apis[0].payload['required_info']
+        required_info = namespace.apis[0].payload["required_info"]
         new_device_info = eval(required_info)
-        name = new_device_info.pop('name', None)
+        name = new_device_info.pop("name", None)
         class_plugin = plugin_manager.get_implementation_of(new_device_info)
         class_plugin.name = name
         device_database.add_device(class_plugin)
         class_plugin.setup()
-        return 'new device', 201
+        return "new device", 201
