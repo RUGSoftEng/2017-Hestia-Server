912b859ae525a3a54097fae3ad0be5de82a4d225
LarsHoldijk <larsholdijk@gmail.com>
2017-04-12 12:55:02 +0200

    util function now also contains documentation

--- source/model/util/stringToBool.py
+++ source/model/util/stringToBool.py
@@ -1,7 +1,5 @@
 def string_to_bool(string):
-    if string == "True" or string == "true":
-        return True
-    elif string == "False" or string == "false":
-        return False
-    else:
-        raise Exception("Invalid conversion to bool form: " + string)
+    """
+    This method can be used to translate a string to a boolean value
+    """
+    return string in ["True", "true", "TRUE"]
