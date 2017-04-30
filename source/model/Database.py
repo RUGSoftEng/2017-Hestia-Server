ad8e688b2a20c24f5a6afe681d6bf065eb714a49
Suzanne Maquelin <slmaquelin@gmail.com>
2017-04-30 15:50:08 +0200

    added tests database and refactored class Database

--- source/model/Database.py
+++ source/model/Database.py
@@ -1,33 +1,32 @@
 class Database:
     """ A singleton that represents our database """
 
-    devices = list()
     _device_counter = 0
 
     def __init__(self):
-        self._device_counter = len(Database.devices)
+        self.devices = list()
+        self._device_counter = len(self.devices)
 
     def get_devices(self):
         """ Get a devices from the Data Access Object """
-        return Database.devices
+        return self.devices
 
     def get_device(self, device_id):
         """ Get a device with a specific id from the Data Access Object """
-        return next(device for device in Database.devices if device.id == device_id)
+        return next(device for device in self.devices if device.id == device_id)
 
     def add_device(self, device):
         """ Add a device to the the Data Access Object """
         device.id = self.get_new_counter()
-        Database.devices.append(device)
+        self.devices.append(device)
 
     def delete_device(self, device_id):
         """ Delete a device with a specific id from the Data Access Object """
-        device = next(device for device in Database.devices if device.id == device_id)
-        Database.devices.remove(device)
+        device = next(device for device in self.devices if device.id == device_id)
+        self.devices.remove(device)
 
-    @classmethod
-    def get_new_counter(cls):
+    def get_new_counter(self):
         """ Get a new counter for a device """
-        new_counter = cls._device_counter
-        cls._device_counter += 1
+        new_counter = self._device_counter
+        self._device_counter += 1
         return new_counter
