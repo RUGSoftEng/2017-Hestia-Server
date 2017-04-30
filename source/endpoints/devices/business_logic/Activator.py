77764ec60bfed2d40cd2eca12a6b08d61395a557
LarsHoldijk <larsholdijk@gmail.com>
2017-04-30 16:29:00 +0200

    Added test for changing the activator state from endpoint

--- source/endpoints/devices/business_logic/Activator.py
+++ source/endpoints/devices/business_logic/Activator.py
@@ -5,7 +5,7 @@ class BusinessLogicActivator:
     def get_activator_from_database_by_device_and_activator_id(self, device_id, activator_id):
         return self._get_activator(device_id, activator_id)
 
-    def change_activator_state(self, device_id, activator_id, state):
+    def change_activator_state_with_string(self, device_id, activator_id, state):
         required_info = self._database.get_device(device_id).required_info
         activator = self._get_activator(device_id, activator_id)
         activator.set_state_with_string(state)
--- source/tests/endpoints/devices/test_activator.py
+++ source/tests/endpoints/devices/test_activator.py
@@ -18,3 +18,20 @@ class TestEndpointActivator(unittest.TestCase):
         activator = lock.get_activator(activator_id)
 
         retrieved_activator = self._logic.get_activator_from_database_by_device_and_activator_id(device_id, activator_id)
+
+        self.assertEqual(activator, retrieved_activator)
+
+    def test_change_activator_state(self):
+        lock = Lock()
+        self._database.add_device(lock)
+        device_id = lock.id
+        activator_id = 0
+        activator = lock.get_activator(activator_id)
+
+        requested_state = True
+        self._logic.change_activator_state_with_string(device_id, activator_id, str(requested_state))
+        self.assertEqual(requested_state, activator.state)
+
+        requested_state = False
+        self._logic.change_activator_state_with_string(device_id, activator_id, str(requested_state))
+        self.assertEqual(requested_state, activator.state)
