24fa2bdc13511cf34eaab2bd2c7276c515e3a1e4
Sebastian Zijerveld <s.zijerveld@gmail.com>
2017-05-01 21:21:38 +0200

    Changed version

--- source/endpoints/api.py
+++ source/endpoints/api.py
@@ -4,7 +4,7 @@ from .devices.Devices import namespace as devices_namespace
 from .plugins.RequiredInfo import namespace as plugin_namespace
 
 api = Api(
-    version='0.3'
+    version='0.4'
     , title='Hestia Home Automation app'
     , description='A home automation hub for all your IOT devices'
 )
