4b6829da78b1596427c34135562a8229ae0154b7
Sebastian Zijerveld <s.zijerveld@gmail.com>
2017-05-11 11:10:10 +0200

    Fix for forgetting to add /

--- source/util/BasePath.py
+++ source/util/BasePath.py
@@ -3,4 +3,4 @@ from os import path
 def get_base_path():
     current_dir_path = path.dirname(__file__)
     base_path = path.dirname(current_dir_path)
-    return base_path
+    return base_path + "/"
