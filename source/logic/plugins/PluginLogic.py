8b15c37e9078db49681afb8cf06bffd03ad488d8
Sebastian Zijerveld <s.zijerveld@gmail.com>
2017-05-14 14:07:23 +0200

    Implemented error messages in plugin endpoint

--- source/logic/plugins/PluginLogic.py
+++ source/logic/plugins/PluginLogic.py
@@ -1,3 +1,7 @@
+from logic.util import abort_with_error
+from util.NotFoundException import NotFoundException
+
+
 class PluginLogic:
     """
     This class holds the logic to retrieve information regarding plugins.
@@ -10,11 +14,17 @@ class PluginLogic:
         return self._plugin_manager.get_organizations()
 
     def get_plugins(self, organization):
-        return self._plugin_manager.get_plugins_of(organization)
+        try:
+            return self._plugin_manager.get_plugins_of(organization)
+        except NotFoundException as exception:
+            abort_with_error(str(exception))
 
     def get_required_info(self, organization, plugin_name):
-        required_info = self._plugin_manager.get_required_info_of(organization, plugin_name)
-        required_info["organization"] = organization
-        required_info["plugin_name"] = plugin_name
-        required_info["name"] = "default"
-        return required_info
+        try:
+            required_info = self._plugin_manager.get_required_info_of(organization, plugin_name)
+            required_info["organization"] = organization
+            required_info["plugin_name"] = plugin_name
+            required_info["name"] = "default"
+            return required_info
+        except NotFoundException as exception:
+            abort_with_error(str(exception))
