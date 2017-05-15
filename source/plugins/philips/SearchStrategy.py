7b2cd97d068198f31cbd636ce3de8f857253044b
Lars Holdijk <larsholdijk@gmail.com>
2017-05-15 12:20:06 +0200

    Update SearchStrategy.py

--- source/plugins/philips/SearchStrategy.py
+++ source/plugins/philips/SearchStrategy.py
@@ -2,6 +2,9 @@ from abc import ABC, abstractmethod
 
 
 class SearchStrategy(ABC):
+    """
+    Classes implementing this abstract classes are used for searching the lamp_id.
+    """
     @classmethod
     @abstractmethod
     def search(cls, response, types):
