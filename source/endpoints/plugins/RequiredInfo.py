4671459641ebd477f8253c85f7711813341e5944
Suzanne Maquelin <slmaquelin@gmail.com>
2017-05-19 16:08:19 +0200

    changed organization to collection in endpoints

--- source/endpoints/plugins/RequiredInfo.py
+++ source/endpoints/plugins/RequiredInfo.py
@@ -5,35 +5,35 @@ from logic import plugin_logic
 
 
 @namespace.route("/")
-class Organizations(Resource):
-    """ Show all the organizations that have plugins """
+class Collections(Resource):
+    """ Show all the collections that have plugins """
 
-    @namespace.doc("list_organizations")
+    @namespace.doc("list_collections")
     def get(self):
-        """ Fetch all the organizations that have plugins installed """
+        """ Fetch all the collections that have plugins installed """
         return plugin_logic.get_organizations()
 
 
-@namespace.route("/<string:organization>/")
-@namespace.response(404, "Organization  not found")
-@namespace.param("organization", "The name of the organization of the plugin")
+@namespace.route("/<string:collections>/")
+@namespace.response(404, "Collection  not found")
+@namespace.param("collection", "The name of the collection of the plugin")
 class Plugins(Resource):
-    """ Show all plugins of an organization """
+    """ Show all plugins of an collection """
 
     @namespace.doc("get_activator")
-    def get(self, organization):
-        """ Fetch all plugins of an organization """
-        return plugin_logic.get_plugins(organization)
+    def get(self, collection):
+        """ Fetch all plugins of an collection """
+        return plugin_logic.get_plugins(collection)
 
 
-@namespace.route("/<string:organization>/plugins/<string:plugin_name>")
-@namespace.response(404, "Organization or plugin not found")
-@namespace.param("organization", "The name of the organization of the plugin")
+@namespace.route("/<string:collection>/plugins/<string:plugin_name>")
+@namespace.response(404, "Collection or plugin not found")
+@namespace.param("collection", "The name of the collection of the plugin")
 @namespace.param("plugin_name", "The name of the plugin")
 class RequiredInfo(Resource):
     """ Show the required information of a plugin """
 
     @namespace.doc("get_activator")
-    def get(self, organization, plugin_name):
-        """ Fetch the required information of a plugin based on its organization and name """
-        return plugin_logic.get_required_info(organization, plugin_name)
+    def get(self, collection, plugin_name):
+        """ Fetch the required information of a plugin based on its collection and name """
+        return plugin_logic.get_required_info(collection, plugin_name)
