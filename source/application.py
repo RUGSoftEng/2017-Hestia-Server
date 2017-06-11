6f7c43de785725065d804971db32e7a85bb4fbec
Lars Holdijk <l.g.holdijk@gmail.com>
2017-06-11 12:47:44 +0200

    Might have fixed local-ip

--- source/application.py
+++ source/application.py
@@ -56,8 +56,11 @@ def _publish_server():
     This makes it possible for the client application to find the ip
     of the server easily."""
     zero_conf = Zeroconf()
-    addr = get_lan_ip()
-    bytes = socket.inet_aton(addr)
+    #addr = get_lan_ip()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 1))  # connect() for UDP doesn't send packets
+    local_ip_address = s.getsockname()[0]
+    bytes = socket.inet_aton("192.168.178.30")
     info = ServiceInfo("_hestia._tcp.local.",
                        "HestiaServer._hestia._tcp.local.",
                        port=8000, properties={'api_level': api.version},
